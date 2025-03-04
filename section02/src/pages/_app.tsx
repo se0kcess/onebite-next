907724618b6033b3e278cfd6b7efbe43ec5b03e2
MinSeok Kim <kimminsuk051@gmail.com>
2025-03-04 17:42:15 +0900

    feat: 페이지라우팅 및 네비게이션 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,10 +1,27 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push('/test'); // .replace => 뒤로가기 방지, .back => 뒤로가기
+  };
+
   return (
     <>
-      <header>글로벌 헤더 </header>
+      <header>
+        <Link href={'/'}>index</Link>
+        &nbsp;
+        <Link href={'/search'}>search</Link>
+        &nbsp;
+        <Link href={'/book/1'}>book1</Link>
+        <div>
+          <button onClick={onClickButton}>/test페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />;
     </>
   );

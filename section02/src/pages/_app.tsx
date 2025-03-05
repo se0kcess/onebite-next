893dbc877bfaa0b31ee1ac620b20881975c76f12
MinSeok Kim <kimminsuk051@gmail.com>
2025-03-05 17:40:40 +0900

    style: 글로벌 레이아웃 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,35 +1,11 @@
+import GlobalLayout from '@/components/global-layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push('/test'); // .replace => 뒤로가기 방지, .back => 뒤로가기
-  };
-
-  useEffect(() => {
-    router.prefetch('/test');
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={'/'}>index</Link>
-        &nbsp;
-        <Link href={'/search'} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={'/book/1'}>book1</Link>
-        <div>
-          <button onClick={onClickButton}>/test페이지로 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />;
-    </>
+    <GlobalLayout>
+      <Component {...pageProps} />
+    </GlobalLayout>
   );
 }
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,7 +1,9 @@
+import style from './index.module.css';
+
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      <h1 className={style.h1}>인덱스</h1>
     </>
   );
 }
